618f8ef06730528f98837f7bc303896feee3923d
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-26 22:42:17 +0100

    protected routes

--- app.js
+++ app.js
@@ -14,45 +14,40 @@ const path = require('path')
 
 const session = require('express-session');	//To Acquire it
 
-app.use(session({ 		//Usuage
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 // app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.get('/users/welcome', (req, res) => {
+app.use('/users', usersRouter)
+
+// Basic Authentication for session and cookies
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    else {
+      res.send('Please login')
+    }
+  }
+
+  app.get('/users/welcome', isLoggedIn, (req, res) => {
+    console.log(req.isAuthenticated())
     console.log(req.user)
     res.json({message : 'Welcome Steve', user : req.user})
 })
 
-
-// Basic Authentication for session and cookies
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-    next();
-  }
-}
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
